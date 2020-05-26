a7bb16514b24564ee882c8f914b5cafbc4818fc8
FabricioMendes <47994791+FabricioMendes@users.noreply.github.com>
2020-05-26 01:59:31 -0300

    Update HoraDoSistema.java

--- HoraDoSistema.java
+++ HoraDoSistema.java
@@ -4,14 +4,12 @@ package horadosistema;
 import java.util.Date;
 
 /**
- *
  * @author Fabricio Mendes
+         26/05/2020
  */
 public class HoraDoSistema {
-
     
     public static void main(String[] args) {
-        
         Date relogio = new Date();
         System.out.println("A hora do sistema é");
         System.out.println(relogio.toString());
