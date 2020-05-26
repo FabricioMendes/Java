a87721c79412a1b256b6d2281339afdb9c0dde31
FabricioMendes <47994791+FabricioMendes@users.noreply.github.com>
2020-05-26 02:02:10 -0300

    Update IndiomaSistema.java

--- IndiomaSistema.java
+++ IndiomaSistema.java
@@ -9,11 +9,11 @@ import java.util.Locale;
 public class IndiomaSistema {
     
     public static void main(String[] args){
-   
-    Locale loc = Locale.getDefault();
-    
-    System.out.println(loc.getDisplayLanguage());
-    
-    System.out.println(loc.getLanguage());   
+
+        Locale loc = Locale.getDefault();
+
+        System.out.println(loc.getDisplayLanguage());
+
+        System.out.println(loc.getLanguage());   
 }
 }
